d9b693162984c9b0c190d4e96cb49013f9053329
Thang Chau <thangnc@uci.edu>
2025-08-01 20:44:39 -0700

    finished tests 3, 4, and 5 for automation exercises

--- Automation Exercise/Test cases/common_functions.py
+++ Automation Exercise/Test cases/common_functions.py
@@ -123,3 +123,64 @@ def delete_account(driver):
     )
     continue_button = driver.find_element(By.XPATH, "//a[@data-qa='continue-button']")
     continue_button.click()
+
+def delete_account_outside_solo_call():
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service) # launch browser
+
+    driver.get("http://automationexercise.com") # navigate to url
+
+    WebDriverWait(driver, 5).until(
+        # verify that home page is visible
+        EC.presence_of_element_located((By.XPATH, "//img[@alt='Website for automation practice']"))
+    )
+    
+    # click 'Delete Account button'
+    delete_account_button = driver.find_element(By.XPATH, "//a[contains(text(), ' Delete Account')]")
+    delete_account_button.click()
+
+    # verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button
+    WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//b[text()='Account Deleted!']"))
+    )
+    continue_button = driver.find_element(By.XPATH, "//a[@data-qa='continue-button']")
+    continue_button.click()
+    
+    driver.quit()
+
+def log_in():
+    service = Service(executable_path="chromedriver.exe")
+    driver = webdriver.Chrome(service=service) # launch browser
+
+    driver.get("http://automationexercise.com") # navigate to url
+
+    WebDriverWait(driver, 5).until(
+        # verify that home page is visible
+        EC.presence_of_element_located((By.XPATH, "//img[@alt='Website for automation practice']"))
+    )
+
+    # click on 'Signup / Login' button
+    login_signup_button = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Signup / Login')]"))
+    )
+    login_signup_button.click()
+
+    WebDriverWait(driver, 5).until(
+        # verify 'Login to your account' is visible
+        EC.visibility_of_element_located((By.XPATH, "//h2[text()='Login to your account']"))
+    )
+
+    # enter correct email address and password
+    email_correct = driver.find_element(By.XPATH, "//input[@data-qa='login-email']")
+    email_correct.send_keys("testemail423@gmail.com")
+    password_correct = driver.find_element(By.XPATH, "//input[@data-qa='login-password']")
+    password_correct.send_keys("!23456A@")
+
+    # click 'Login' button
+    login_button = driver.find_element(By.XPATH, "//button[@data-qa='login-button']")
+    login_button.click()
+
+    # verify that 'Logged in as username' is visible
+    verify_username_is_visible(driver)
+
+    driver.quit()
